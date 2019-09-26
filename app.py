3b4dca5a4beb080ee97873513b788f54d36165a4
ey7 <eoinyoung84@gmail.com>
2019-09-26 22:01:18 +0100

    added code mods to display data to users.html

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for, flash, session
-from flask_pymongo import PyMongo, pymongo
+from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from config import Config
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# environment variables
-app.config["MONGO_DBNAME"] = "flaskBlog"
-app.config["MONGO_URI"] = os.getenv("MONGO_URI")
-mongo = PyMongo(app)
+
+# connection to mongoDB and set database
+client = MongoClient(Config.MONGO_URI)
+print("Mongo is connected!")
+db = client.flaskBlog
+
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+	return render_template('home.html')
 
-@app.route("/user/<username>")
-def user_profile(username):
-    user = mongo.db.blogs.find_one_or_404({"_id": password})
-    return render_template("users.html",
-        user=user)    
+@app.route("/users/")
+def user_profile():
+	user = db.blogs.find_one({"_id": ObjectId('5d8a95cf1c9d440000ed6705')})
+	return render_template('users.html', user=user)    
 
 @app.route('/login')
 def login():
@@ -26,5 +29,5 @@ def login():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)  
+	app.run(debug=True)  
 
