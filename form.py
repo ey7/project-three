ceb3b53d07cabc1a3d19956ce97069048f845c7a
ey7 <eoinyoung84@gmail.com>
2019-09-28 18:25:35 +0100

    login class added to form.py

--- form.py
+++ form.py
@@ -2,11 +2,20 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo
 
+#  class for registration form
 class UsernamePasswordConfirm(FlaskForm):
 
     username = StringField('Username', validators=[DataRequired(),
-                           Length(min=2, max=20)])
+                           Length(min=4, max=20)])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
             validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+#class for login form
+class UsernamePassword(FlaskForm):
+
+    username = StringField('Username', validators=[DataRequired(),
+                           Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
