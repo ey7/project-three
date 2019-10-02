ca8ba6c92698afb4e26609c13459ff2a0a8da322
ey7 <eoinyoung84@gmail.com>
2019-10-02 11:11:05 +0100

    added new article form class

--- form.py
+++ form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 #  class for registration form
@@ -19,3 +19,11 @@ class UsernamePassword(FlaskForm):
                            Length(min=4, max=20)])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+# class for article form
+class ContentTitleForm(FlaskForm):
+
+    title = StringField('Title,' validators=[DataRequired(),
+        Length(min=1. max=150)])
+    content = TextAreaField('Content', validators=[DataRequired(),
+        Length(min=50)])
